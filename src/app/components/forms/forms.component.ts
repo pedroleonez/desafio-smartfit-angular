28039f221341f301cf78f565cf33beb6db8e4116
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-03-29 13:51:42 -0300

    feat: add ReactiveFormsModule and methods ngOnInit, onSubmit and onClean

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent {
 
   results = [];
+  formGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
 
 }
